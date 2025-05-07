ca9d6b898fcebe48d704866bf745267c0d16cd09
Duong Nhat Minh <nhatminh.st.04@gmail.com>
2025-05-07 16:05:37 +0700

    dang nhap

--- src/main/java/com/nminh/kiemthu/enums/ErrorCode.java
+++ src/main/java/com/nminh/kiemthu/enums/ErrorCode.java
@@ -5,7 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 @Getter
 public enum ErrorCode {
-    USER_EXISTS(1001,"user existed" , HttpStatus.BAD_REQUEST)
+    USER_EXISTS(1001,"user existed" , HttpStatus.BAD_REQUEST),
+    USER_NOT_EXISTS(1002,"user not existed" , HttpStatus.BAD_REQUEST),
+    PASSWORD_ERROR(1003,"password error" , HttpStatus.BAD_REQUEST),
     ;
 
     private int code ;
---
+++ src/main/java/com/nminh/kiemthu/model/request/UserLoginDTO.java
@@ -0,0 +1,9 @@
+package com.nminh.kiemthu.model.request;
+
+import lombok.Data;
+
+@Data
+public class UserLoginDTO {
+    private String username;
+    private String password;
+}
